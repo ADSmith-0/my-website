f3ca95cd21d730ed556f2710f6b31eedc7c506e9
adam smith <adam00199382818@gmail.com>
2024-03-20 11:51:28 +0000

    added navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,42 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const navItems = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "Projects",
+      path: "/projects",
+    },
+    {
+      id: 3,
+      name: "About me",
+      path: "/about",
+    },
+    {
+      id: 4,
+      name: "Hot takes",
+      path: "/hot_takes",
+    },
+  ];
+</script>
+
 <nav>
-  <a href="/">Home</a>
-  <a href="/projects">Projects</a>
-  <a href="/about">About me</a>
-  <a href="/hot_takes">Hot takes</a>
+  {#each navItems as navItem (navItem.id)}
+    <a href={navItem.path} class:highlighted={$page.route.id === navItem.path}
+      >{navItem.name}</a
+    >
+  {/each}
 </nav>
 
 <slot />
+
+<style>
+  .highlighted {
+    color: red;
+  }
+</style>
