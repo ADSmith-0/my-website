04bab9a6f7306de0466f75f176dd0d133a7fe8a6
adam smith <adam00199382818@gmail.com>
2024-03-28 00:45:46 +0000

    added link to images and bottom link to projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
+  import { ChevronsRight } from "lucide-svelte";
   import type { PageData } from "./$types";
   export let data: PageData;
-
-  import Card from "$lib/components/Card.svelte";
 </script>
 
 <div class="flex-column-center margin-auto mt-10">
@@ -11,8 +10,44 @@
     class="flex-row-center flex-wrap gap-8 justify-content-center"
     style="max-width: 1050px;"
   >
-    {#each data.projects as project (project.id)}
-      <Card src={project.src} alt={project.alt} />
+    {#each data.projects as { src, alt, id, link } (id)}
+      <a href={link}>
+        <img
+          src="/images/{src}"
+          {alt}
+          class="border-1 br-1 border-grey-600 cursor-pointer corner"
+        />
+      </a>
     {/each}
   </div>
+  <a href="/projects" class="flex-row-center gap-2 mt-10 pt-10 pb-2 link"
+    >View More Projects <ChevronsRight
+      color="var(--grey-200)"
+      size="var(--font-size-m)"
+    /></a
+  >
 </div>
+
+<style>
+  .link {
+    text-decoration: none;
+    position: relative;
+  }
+  .link:visited {
+    color: var(--grey-200);
+  }
+  .link::after {
+    content: "";
+    display: block;
+    bottom: 0;
+    left: 0;
+    height: var(--spacing-1);
+    position: absolute;
+    width: 0;
+    background-color: var(--grey-200);
+    transition: width 0.2s ease-out;
+  }
+  .link:hover::after {
+    width: 100%;
+  }
+</style>
