5f9581c3b76e1beb18ec270ca5b747e3f37ffd03
adam smith <adam00199382818@gmail.com>
2024-05-07 11:04:39 +0100

    updated grately icon

--- src/lib/icons/GratelyIcon.svelte
+++ src/lib/icons/GratelyIcon.svelte
@@ -4,7 +4,7 @@
     <circle cx="55" cy="20" r="5" />
   </g>
   <path
-    d="M 25 40 C 15 70, 65 70, 55 40 L 38 40"
+    d="M 25.5 38.5 C 15 71, 65 70, 55 40 L 38 40"
     stroke="black"
     fill="transparent"
     stroke-width="7"
