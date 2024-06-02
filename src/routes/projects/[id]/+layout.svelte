6d916f82fa55b8febd958fdd1994e8605e2694d2
adam smith <adam00199382818@gmail.com>
2024-06-02 18:02:05 +0100

    added highlighting for what project you were currently viewing

--- src/routes/projects/[id]/+layout.svelte
+++ src/routes/projects/[id]/+layout.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 import Page from "$lib/Page.svelte";
 import { projects } from "$lib/projects";
+import type { PageData } from "./$types";
+
+export let data: PageData;
 </script>
 
 <Page id="projects-info">
   <aside class="ph-8 bri-1 border-grey-600 flex-column gap-5">
-    <a href="/" class="link fs-xl mb-5 align-self-start">&lt; Back</a>
+    <a href="/" class="link fs-xl mt-3 mb-5 align-self-start">&lt; Back</a>
     {#each projects as { name, link } (name)}
-      <a href={`/projects/${link}`} class="fs-l">{name}</a>
+      <a
+        href={`/projects/${link}`}
+        class="fs-l"
+        class:highlighted={link === data.path}>{name}</a
+      >
     {/each}
   </aside>
   <main class="p-5 pl-10">
@@ -21,5 +28,9 @@ import { projects } from "$lib/projects";
     grid-template-columns: minmax(200px, auto) 1fr;
     align-items: stretch;
     justify-items: start;
+  }
+  .highlighted {
+    font-weight: bold;
+    color: var(--orange);
   }
 </style>
