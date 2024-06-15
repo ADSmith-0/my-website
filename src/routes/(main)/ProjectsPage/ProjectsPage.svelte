8992b7ea4a9270b5f085ec40a0aec585ee70f447
adam smith <adam00199382818@gmail.com>
2024-06-16 00:47:48 +0100

    added right arrow to project link on hover

--- src/routes/(main)/ProjectsPage/ProjectsPage.svelte
+++ src/routes/(main)/ProjectsPage/ProjectsPage.svelte
@@ -8,10 +8,8 @@ import NeovimIcon from "$lib/icons/NeovimIcon.svelte";
 import PokedexIcon from "$lib/icons/PokedexIcon.svelte";
 import SvelteIcon from "$lib/icons/SvelteIcon.svelte";
 import { projects } from "$lib/projects";
-import { ArrowRight } from "lucide-svelte";
 import type { ComponentType } from "svelte";
 import Card from "./Card.svelte";
-import { fly } from "svelte/transition";
 
 const components: Record<string, ComponentType> = {
 	GratelyIcon,
@@ -22,32 +20,12 @@ const components: Record<string, ComponentType> = {
 	PokedexIcon,
 	SvelteIcon,
 };
-
-let hovering = false;
-
-const setHovering = (newHovering: boolean): void => {
-	hovering = newHovering;
-};
 </script>
 
 <Page id="projects">
-  <h2
-    class="mt-5 mb-10 link"
-    on:mouseover={() => setHovering(true)}
-    on:focus={() => setHovering(true)}
-    on:mouseleave={() => setHovering(false)}
-    on:blur={() => setHovering(false)}
-  >
-    <a href="/projects/grately" class="fs-3xl place-self-center"
-      ><span>Projects</span>
-      {#if hovering}
-        <span
-          style="display: inline-block;"
-          transition:fly={{ x: -20, duration: 400 }}
-        >
-          <ArrowRight />
-        </span>
-      {/if}
+  <h2 class="mt-5 mb-10">
+    <a id="title" href="/projects/grately" class="fs-3xl place-self-center link"
+      >Projects
     </a>
   </h2>
   <div
@@ -67,3 +45,25 @@ const setHovering = (newHovering: boolean): void => {
     {/each}
   </div>
 </Page>
+
+<style>
+  #title {
+    position: relative;
+    color: var(--grey-200);
+  }
+
+  #title::after {
+    position: absolute;
+    display: inline-block;
+    content: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgY2xhc3M9Imx1Y2lkZSBsdWNpZGUtYXJyb3ctcmlnaHQiIHN0cm9rZT0iI2UxZGJkNiI+PHBhdGggZD0iTTUgMTJoMTQiLz48cGF0aCBkPSJtMTIgNSA3IDctNyA3Ii8+PC9zdmc+);
+    top: calc(-1 * var(--spacing-2));
+    right: 0;
+    opacity: 0;
+    transition: all linear 0.3s;
+  }
+
+  #title:hover::after {
+    transform: translateX(var(--spacing-8));
+    opacity: 1;
+  }
+</style>
