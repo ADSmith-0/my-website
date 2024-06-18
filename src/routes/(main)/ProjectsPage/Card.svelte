1c1eaa2f86cefef705c811c7ad31595447d948ca
adam smith <adam00199382818@gmail.com>
2024-06-18 11:29:33 +0100

    changed to use new hover animation

--- src/routes/(main)/ProjectsPage/Card.svelte
+++ src/routes/(main)/ProjectsPage/Card.svelte
@@ -4,16 +4,27 @@ export let text: string;
 export let iconSize: number;
 export let borderColour = "#0cf";
 // FIX: https://www.youtube.com/watch?v=fcnDBP3k3BE - Change border with this
+
+const containerOffset = "100px";
+const glowOffset = "1px";
+const borderRadius = "4px";
 </script>
 
 <a
+  id="container"
   href={link}
   target={link[0] !== "/" ? "_blank" : undefined}
   rel="noopener noreferrer"
-  class="flex-row-center justify-content-center card p-5 br-1 border-box cursor-pointer animated-border"
+  class="flex-row-center justify-content-center br-1 cursor-pointer animated-border glow-effect"
   style:--border-colour={borderColour}
+  style:--container-offset={containerOffset}
+  style:--glow-offset={glowOffset}
+  style:--border-radius={borderRadius}
 >
-  <div class="flex-row-center justify-content-center gap-5">
+  <div
+    id="card"
+    class="flex-row-center justify-content-center gap-5 p-5 w-100 h-100 border-box"
+  >
     <span
       class="flex-row-center justify-content-center"
       style="height: {iconSize}px; width: {iconSize}px;"
@@ -24,52 +35,80 @@ export let borderColour = "#0cf";
       {text}
     </h3>
   </div>
+  <svg id="glow-container">
+    <rect
+      id="glow-line"
+      pathLength="100"
+      x={`${parseInt(containerOffset) / 2 + parseInt(glowOffset) / -2}px`}
+      y={`${parseInt(containerOffset) / 2 + parseInt(glowOffset) / -2}px`}
+      stroke-linecap="round"
+      rx={borderRadius}
+    ></rect>
+    <rect
+      id="glow-blur"
+      pathLength="100"
+      x={`${parseInt(containerOffset) / 2 + parseInt(glowOffset) / -2}px`}
+      y={`${parseInt(containerOffset) / 2 + parseInt(glowOffset) / -2}px`}
+      stroke-linecap="round"
+      rx={borderRadius}
+    ></rect>
+  </svg>
 </a>
 
 <style>
-  .card {
+  #container {
+    --glow-line-thickness: var(--spacing-1);
+    --glow-blur-size: var(--spacing-3);
+    --animation-speed: 2s;
+    /* do not change, used for calculations */
+    --container-offset: 100px;
+    --height: calc(var(--project-card-size) * 9 / 16);
     position: relative;
     width: var(--project-card-size);
-    aspect-ratio: 16 / 9;
-    overflow: hidden;
-    transition: transform 0.2s linear;
+    height: var(--height);
+    border-radius: var(--border-radius);
+    transition: transform 0.1s linear;
   }
-  .card:hover {
-    transform: scale(1.05);
+  #container:is(:hover, :focus) {
+    transform: scale(1.02);
   }
-  .card div {
-    position: absolute;
-    inset: 2px;
-    z-index: 10;
+  #card {
     border-radius: inherit;
     background: radial-gradient(var(--grey-900), var(--grey-950));
+    border: var(--spacing-1) solid var(--border-colour);
+    transition: border-color linear 0.4s;
   }
-  .card:hover div {
-    transform: translateX(0.5px);
-  }
-  .card::before,
-  .card::after {
-    content: "";
+  #glow-container {
+    pointer-events: none;
     position: absolute;
-    width: 345px;
-    height: 345px;
-    border-radius: inherit;
-    background: var(--border-colour);
-    animation: rotate 6s linear infinite;
+    inset: calc(var(--container-offset) / -2);
+    width: calc(100% + var(--container-offset));
+    height: calc(100% + var(--container-offset));
   }
-  .card::after {
-    animation-delay: -3s;
+  #glow-line,
+  #glow-blur {
+    width: calc(100% - var(--container-offset) + var(--glow-offset));
+    height: calc(100% - var(--container-offset) + var(--glow-offset));
+    opacity: 0;
+    fill: transparent;
+    stroke: var(--border-colour);
+    stroke-width: var(--glow-line-thickness);
+    stroke-dasharray: 20px 30px;
+    transition: all 0.1s linear;
   }
-  .card:hover::before,
-  .card:hover::after {
-    background: conic-gradient(transparent, transparent, var(--border-colour));
+  #glow-blur {
+    filter: blur(var(--glow-blur-size));
   }
-  @keyframes rotate {
+  #container:is(:hover, :focus) :is(#glow-line, #glow-blur) {
+    opacity: 1;
+    animation: spin var(--animation-speed) linear infinite;
+  }
+  @keyframes spin {
     0% {
-      transform: rotate(0deg);
+      stroke-dashoffset: 0px;
     }
     100% {
-      transform: rotate(360deg);
+      stroke-dashoffset: -50px;
     }
   }
   .text-shadow {
